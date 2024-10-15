4fb7693f44d9e81df76b89c7a12bef5a79a810db
oliverrich <oliverrichards12@gmail.com>
2024-10-14 23:24:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,53 +3,20 @@ using namespace std;
 
 
 int main(){
-    int* intArray = nullptr;
+    int* intArray = new int[1];
     int currentSize = 0;
     int maxSize = 1;
     int input;
 
-
-    intArray = new int[maxSize];
-
     cout << "Enter values for the array (enter anything but an integer to stop):" << endl;
 
-
     while (cin >> input) {
-
-        if (currentSize == maxSize) {
-            // Double the capacity
-            maxSize *= 2;
-
-
-            int* tempArray = new int[maxSize];
-
-
-            for (int i = 0; i < currentSize; i++) {
-                tempArray[i] = intArray[i];
-            }
-
-
-            delete[] intArray;
-
-
-            intArray = tempArray;
-        }
-
-
-        intArray[currentSize] = input;
-        currentSize++;
+        addElement(intArray, currentSize, maxSize, input);
     }
 
-
-    int sum = 0;
-    for (int i = 0; i < currentSize; i++) {
-        sum += intArray[i];
-    }
-
+    int sum = sumArray(intArray, currentSize);
     cout << "The sum of the array is: " << sum << endl;
 
-
     delete[] intArray;
-
     return 0;
 }
